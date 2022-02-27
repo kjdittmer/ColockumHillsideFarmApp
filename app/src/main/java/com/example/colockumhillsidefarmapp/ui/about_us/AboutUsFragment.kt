6345093b7330e0f271fb50db0af9938f600a70bc
kjdittmer <59971751+kjdittmer@users.noreply.github.com>
2022-02-27 00:42:37 -0600

    Attempted to add tabs to about us page, with mixed success

--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/AboutUsFragment.kt
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/AboutUsFragment.kt
@@ -6,37 +6,62 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.colockumhillsidefarmapp.databinding.FragmentAboutUsBinding
 
+/**
+ * A placeholder fragment containing a simple view.
+ */
 class AboutUsFragment : Fragment() {
 
+    private lateinit var pageViewModel: com.example.colockumhillsidefarmapp.ui.about_us.PageViewModel
     private var _binding: FragmentAboutUsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        pageViewModel = ViewModelProvider(this).get(PageViewModel::class.java).apply {
+            setIndex(arguments?.getInt(ARG_SECTION_NUMBER) ?: 1)
+        }
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(AboutUsViewModel::class.java)
+    ): View? {
 
         _binding = FragmentAboutUsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val root = binding.root
 
         val textView: TextView = binding.textAboutUs
-        galleryViewModel.text.observe(viewLifecycleOwner) {
+        pageViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
-        }
+        })
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    companion object {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private const val ARG_SECTION_NUMBER = "section_number"
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        @JvmStatic
+        fun newInstance(sectionNumber: Int): AboutUsFragment {
+            return AboutUsFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_SECTION_NUMBER, sectionNumber)
+                }
+            }
+        }
     }
 }
