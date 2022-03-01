52e8405916b2a7d8300fd2ee5756b2994495e889
dlee371 <61858877+dlee371@users.noreply.github.com>
2022-02-28 23:01:35 -0600

    added the option for the vendor to go back to the menu

--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/VendorChoiceActivity.java
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/VendorChoiceActivity.java
@@ -2,6 +2,7 @@ package com.example.colockumhillsidefarmapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.colockumhillsidefarmapp.R;
+import com.example.colockumhillsidefarmapp.ui.about_us.AboutUsFragment;
+import com.example.colockumhillsidefarmapp.ui.about_us.AboutUsViewModel;
 
 public class VendorChoiceActivity extends AppCompatActivity {
 
@@ -42,12 +45,20 @@ public class VendorChoiceActivity extends AppCompatActivity {
                     case R.id.rbUpdateAboutUs:
                         Toast.makeText(VendorChoiceActivity.this, "Go to update about us", Toast.LENGTH_SHORT).show();
                         break;
+                    case R.id.rbMenuButton:
+                        Toast.makeText(VendorChoiceActivity.this, "Go back to menu", Toast.LENGTH_SHORT).show();
+                        openMenuAct();
+                        break;
                     default:
                         break;
                 }
             }
         });
 
+    }
 
+    private void openMenuAct() {
+        Intent intent = new Intent(this, AboutUsViewModel.class);
+        startActivity(intent);
     }
 }
