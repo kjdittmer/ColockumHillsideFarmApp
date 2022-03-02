e9086fcbfba743225f15911da6da66b008a7ec5b
kjdittmer <59971751+kjdittmer@users.noreply.github.com>
2022-03-01 18:26:12 -0600

    Updates

--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/PoultryActivity.kt
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/PoultryActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.colockumhillsidefarmapp.MainActivity
 import com.example.colockumhillsidefarmapp.R
