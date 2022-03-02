f4a3f7c79dcb6bf0d78d3a59db42f9b8edd96c69
kjdittmer <59971751+kjdittmer@users.noreply.github.com>
2022-03-01 18:48:06 -0600

    Veggie Page

--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/PoultryActivity.kt
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/PoultryActivity.kt
@@ -34,7 +34,7 @@ class PoultryActivity: AppCompatActivity() {
         }
         turkey.setOnClickListener{
             this?.let {
-                    it1 -> Snackbar.make(it1.findViewById(R.id.poultryandeggs),
+                    it1 -> Snackbar.make(it1.findViewById(R.id.turkey),
                 "We raise specialty “Midget White” turkeys each year for Thanksgiving. Midget Whites love to spend their day foraging, so on our farm they enjoy a diet of free range shrub steppe findings supplemented with locally sourced, non-GMO, corn- and soy-free feed. This sought after breed takes about 7 months to grow to a compact 8-18 pounds, but rewards the chef with rich and juicy flavor. Watch our store for the opportunity to reserve your Thanksgiving bird!",
                 Snackbar.LENGTH_LONG).apply{view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines=15 }
                 .show()
@@ -42,7 +42,7 @@ class PoultryActivity: AppCompatActivity() {
             }
         eggs.setOnClickListener{
             this?.let {
-                    it1 -> Snackbar.make(it1.findViewById(R.id.poultryandeggs),
+                    it2 -> Snackbar.make(it2.findViewById(R.id.eggs),
                "What makes our Shrub Steppe eggs special? Our hens enjoy the freedom to forage on our 80 acres of pristine shrub steppe land. They spend their days scratching for shoots, leaves and insects. When you crack the eggs open, the benefits of their footloose and fancy-free lifestyle is obvious: the eggs’ yolks are a deep golden orange, and the whites are firm, not runny.",
                 Snackbar.LENGTH_LONG).apply{view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines=15 }
                 .show()
@@ -50,7 +50,7 @@ class PoultryActivity: AppCompatActivity() {
         }
         chicken.setOnClickListener{
             this?.let {
-                    it1 -> Snackbar.make(it1.findViewById(R.id.poultryandeggs),
+                    it3 -> Snackbar.make(it3.findViewById(R.id.chicken),
                 "Our Freedom Ranger chickens are raised on pasture and fed locally-sourced corn-, soy-, and GMO-free feed. This breed is said to have been preferred by Julia Child, and we agree with Julia! Freedom Rangers have a natural balance between white and dark meat and a deep, rich chicken flavor. The meat is juicy and firm–but not the least bit tough",
                 Snackbar.LENGTH_LONG).apply{view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines=10 }
                 .show()
--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/VegetableActivity.kt
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/about_us/VegetableActivity.kt
@@ -1,14 +1,60 @@
 package com.example.colockumhillsidefarmapp.ui.about_us
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.colockumhillsidefarmapp.databinding.VegetableActivityBinding
 import com.example.colockumhillsidefarmapp.R
+import com.google.android.material.snackbar.Snackbar
 
 class VegetableActivity: AppCompatActivity() {
     private lateinit var binding: VegetableActivityBinding
+    private lateinit var veggies: Button
+    private lateinit var microgreens: Button
+    private lateinit var plantstarts: Button
+    private lateinit var practices: Button
+    private lateinit var back: Button
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.vegetable_activity)
+        var root: View = binding.root
+        veggies = root.findViewById(R.id.veggies)
+        microgreens = root.findViewById(R.id.microgreens)
+        plantstarts = root.findViewById(R.id.plantstarts)
+        practices = root.findViewById(R.id.veggiepractices)
+        back = root.findViewById(R.id.back3)
+        veggies.setOnClickListener {
+            this?.let {
+                    it1 -> Snackbar.make(it1.findViewById(R.id.veggies),
+              "You can taste the difference! We use organic practices to grow beautiful, nutrient-rich produce. Compost built on our farm nourishes the soil, and we employ crop rotation, interplanting and water-wise practices to conserve water and nurture our plants and growing beds.    ",
+               Snackbar.LENGTH_LONG).apply{view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines=15 }
+                .show()
+            }
+        }
+        microgreens.setOnClickListener{
+            this?.let {
+                    it2 -> Snackbar.make(it2.findViewById(R.id.microgreens),
+                "Want a delicious boost to your nutrition? We offer a wide variety of microgreens, which pack a powerful antioxidant punch! Microgreens are rich in potassium, iron, zinc, magnesium, copper and polyphenols. We grow our microgreens in an organic soil mix fortified with organic compost and liquid kelp for maximum nutrition. Our microgreens are fresh, colorful and tasty.  Enjoy them in sandwiches, salads, pestos and wraps, or use them as a garnish on any savory dish!",
+                Snackbar.LENGTH_LONG).apply{view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines=15 }
+                .show()
+            }
+        }
+        plantstarts.setOnClickListener{
+            this?.let {
+                    it3 -> Snackbar.make(it3.findViewById(R.id.microgreens),
+               "We sell a wide variety of vegetable starts that we have tested and grown successfully in our own garden beds. Our seeds come from reputable sellers and we make our own organic potting soil. Customers compliment us on the beauty, vigor, and success of our plant starts. ",
+                Snackbar.LENGTH_LONG).apply{view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines=15 }
+                .show()
+            }
+        }
+        practices.setOnClickListener{
+
+        }
+        back.setOnClickListener{
+
+        }
     }
 }
