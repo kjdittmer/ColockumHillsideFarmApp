ad68e60b4d059f2711d25c0bfa97f0a8c059c979
kjdittmer <59971751+kjdittmer@users.noreply.github.com>
2022-03-23 20:55:04 -0500

    Got database working

--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/vendor/AddProductActivity.java
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/vendor/AddProductActivity.java
@@ -1,5 +1,6 @@
 package com.example.colockumhillsidefarmapp.ui.vendor;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,17 +14,26 @@ import android.widget.Toast;
 import com.example.colockumhillsidefarmapp.Product;
 import com.example.colockumhillsidefarmapp.R;
 import com.example.colockumhillsidefarmapp.ShoppingCart;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class AddProductActivity extends AppCompatActivity {
-
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    StoreItem storeItem;
     EditText txtNameAddProdAct, txtQuantityAddProdAct, txtImageUrlProdAct, txtShortDescAddProdAct, txtLongDescAddProdAct, txtPriceAddProdAct, txtPackageQuantityAddProdAct;
     Button btnAddProductAddProdAct;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.colockumhillsidefarmapp.R.layout.activity_add_product);
-
+      firebaseDatabase = FirebaseDatabase.getInstance();
+      databaseReference= firebaseDatabase.getReference("storeItems");
+      storeItem = new StoreItem();
         initVariables();
 
         btnAddProductAddProdAct.setOnClickListener(new View.OnClickListener() {
@@ -43,6 +53,9 @@ public class AddProductActivity extends AppCompatActivity {
                     builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
+
+                            addDataToFirebase(name, imageUrl, shortDesc, longDesc, packageQuantity, quantity, price);
+
                             int newProductId = ShoppingCart.getInstance().getNewId();
                             Product productToAdd = new Product(newProductId, name, quantity, imageUrl, shortDesc, longDesc, price, packageQuantity);
                             ShoppingCart.getInstance().addProductToAllProducts(productToAdd);
@@ -66,6 +79,27 @@ public class AddProductActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void addDataToFirebase(String name, String imageUrl, String shortDesc, String longDesc, String packageQuantity, int quantity, double price) {
+        storeItem.setName(name);
+        storeItem.setImageUrl(imageUrl);
+        storeItem.setShortDesc(shortDesc);
+        storeItem.setLongDesc(longDesc);
+        storeItem.setPackageQuantity(packageQuantity);
+        storeItem.setQuantity(quantity);
+        storeItem.setPrice(price);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                databaseReference.setValue(storeItem);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private boolean validateData() {
