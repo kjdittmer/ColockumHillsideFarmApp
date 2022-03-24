f7dfb01532d7e7bb5b21334a4157e620a4ebde3c
dlee371 <61858877+dlee371@users.noreply.github.com>
2022-03-23 22:12:09 -0500

    need help with connecting java code to main kotlin file within recipes

--- app/src/main/java/com/example/colockumhillsidefarmapp/ui/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/colockumhillsidefarmapp/ui/recipes/RecipesFragment.kt
@@ -1,13 +1,17 @@
 package com.example.colockumhillsidefarmapp.ui.recipes
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.colockumhillsidefarmapp.R
 import com.example.colockumhillsidefarmapp.databinding.FragmentRecipesBinding
+import com.example.colockumhillsidefarmapp.ui.recipes.RecipeActivity
 
 class RecipesFragment : Fragment() {
 
@@ -17,6 +21,7 @@ class RecipesFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private lateinit var recipe: Button
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,10 +33,25 @@ class RecipesFragment : Fragment() {
         _binding = FragmentRecipesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
+
+//        val textView: TextView = binding.textRecipes
+//        homeViewModel.text.observe(viewLifecycleOwner) {
+//            textView.text = it
+//        }
+
         val textView: TextView = binding.textRecipes
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+//        recipe = root.findViewById(R.id.recipeRecView);
+//        recipe.setOnClickListener {
+//            val intent = Intent(activity, RecipeActivity::class.java)
+//            startActivity(intent)
+//
+//        }
+
         return root
     }
 
