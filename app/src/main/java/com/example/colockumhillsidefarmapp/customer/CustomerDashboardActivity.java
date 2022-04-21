2d732a7922e141ee3cda457560fb96e18caeb80c
nicobradley22 <nico.bradley@my.wheaton.edu>
2022-04-20 20:12:22 -0500

    new structure with bottom nav bars

--- app/src/main/java/com/example/colockumhillsidefarmapp/customer/CustomerDashboardActivity.java
+++ app/src/main/java/com/example/colockumhillsidefarmapp/customer/CustomerDashboardActivity.java
@@ -3,6 +3,7 @@ package com.example.colockumhillsidefarmapp.customer;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -10,10 +11,19 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.colockumhillsidefarmapp.AnalyticsFragment;
+import com.example.colockumhillsidefarmapp.CustomerAboutUsFragment;
+import com.example.colockumhillsidefarmapp.CustomerContactUsFragment;
+import com.example.colockumhillsidefarmapp.CustomerRecipesFragment;
+import com.example.colockumhillsidefarmapp.CustomerStoreFragment;
 import com.example.colockumhillsidefarmapp.GlobalStorage;
 import com.example.colockumhillsidefarmapp.R;
+import com.example.colockumhillsidefarmapp.UpdateAboutUsFragment;
+import com.example.colockumhillsidefarmapp.UpdateRecipesFragment;
+import com.example.colockumhillsidefarmapp.UpdateStoreFragment;
 import com.example.colockumhillsidefarmapp.customer.store.Product;
 import com.example.colockumhillsidefarmapp.vendor.update_store.UpdateStoreActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CustomerDashboardActivity extends AppCompatActivity {
 
@@ -23,6 +33,36 @@ public class CustomerDashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_customer_dashboard);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_customer_dashboard);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_customer_store:
+                        selectedFragment = new CustomerStoreFragment();
+                        break;
+                    case R.id.nav_customer_recipes:
+                        selectedFragment = new CustomerRecipesFragment();
+                        break;
+                    case R.id.nav_customer_about_us:
+                        selectedFragment = new CustomerAboutUsFragment();
+                        break;
+                    case R.id.nav_customer_contact_us:
+                        selectedFragment = new CustomerContactUsFragment() ;
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentCustomerDashboard,
+                        selectedFragment).commit();
+                return true;
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.fragmentCustomerDashboard, new CustomerStoreFragment()).
+                commit();
     }
 
     @Override
--- app/src/main/java/com/example/colockumhillsidefarmapp/vendor/VendorDashboardActivity.java
+++ app/src/main/java/com/example/colockumhillsidefarmapp/vendor/VendorDashboardActivity.java
@@ -3,12 +3,18 @@ package com.example.colockumhillsidefarmapp.vendor;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import com.example.colockumhillsidefarmapp.AnalyticsFragment;
 import com.example.colockumhillsidefarmapp.R;
+import com.example.colockumhillsidefarmapp.UpdateAboutUsFragment;
+import com.example.colockumhillsidefarmapp.UpdateRecipesFragment;
+import com.example.colockumhillsidefarmapp.UpdateStoreFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class VendorDashboardActivity extends AppCompatActivity {
 
@@ -18,6 +24,36 @@ public class VendorDashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_vendor_dashboard);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation_vendor_dashboard);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.nav_update_store:
+                        selectedFragment = new UpdateStoreFragment();
+                        break;
+                    case R.id.nav_update_recipes:
+                        selectedFragment = new UpdateRecipesFragment();
+                        break;
+                    case R.id.nav_update_about_us:
+                        selectedFragment = new UpdateAboutUsFragment();
+                        break;
+                    case R.id.nav_analytics:
+                        selectedFragment = new AnalyticsFragment() ;
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentVendorDashboard,
+                        selectedFragment).commit();
+                 return true;
+            }
+        });
+
+        getSupportFragmentManager().beginTransaction().
+                replace(R.id.fragmentVendorDashboard, new UpdateStoreFragment()).
+                commit();
     }
 
     @Override
