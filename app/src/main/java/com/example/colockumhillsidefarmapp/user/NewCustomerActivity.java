661e9e868955f0f348ff692c0f94c3776a09420f
nicobradley22 <nico.bradley@my.wheaton.edu>
2022-04-21 23:02:19 -0500

    create accounts set up with firebase

--- app/src/main/java/com/example/colockumhillsidefarmapp/user/NewCustomerActivity.java
+++ app/src/main/java/com/example/colockumhillsidefarmapp/user/NewCustomerActivity.java
@@ -5,19 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.example.colockumhillsidefarmapp.MainActivity;
 import com.example.colockumhillsidefarmapp.R;
 import com.example.colockumhillsidefarmapp.customer.CustomerDashboardActivity;
+import com.example.colockumhillsidefarmapp.customer.recipes.Recipe;
+import com.example.colockumhillsidefarmapp.customer.store.Product;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 public class NewCustomerActivity extends AppCompatActivity {
 
-    private EditText txtEmail, txtPassword, txtReenterPassword;
+    private EditText txtEmail, txtPassword, txtReenterPassword, txtFullName, txtAge;
     private Button btnCreateAccount;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +45,7 @@ public class NewCustomerActivity extends AppCompatActivity {
         btnCreateAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MainActivity.class);
-                startActivity(intent);
+                registerUser();
             }
         });
     }
@@ -42,6 +55,10 @@ public class NewCustomerActivity extends AppCompatActivity {
         txtPassword = findViewById(R.id.txtPasswordNewCustomer);
         txtReenterPassword = findViewById(R.id.txtReenterPasswordNewCustomer);
         btnCreateAccount = findViewById(R.id.btnCreateAccountNewCustomer);
+        txtFullName = findViewById(R.id.txtNameNewCustomer);
+        txtAge = findViewById(R.id.txtAgeNewCustomer);
+        progressBar = findViewById(R.id.progressBarNewCustomer);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -60,5 +77,109 @@ public class NewCustomerActivity extends AppCompatActivity {
     public void onBackPressed() {
         Intent intent = new Intent(this, CustomerLoginActivity.class);
         startActivity(intent);
+    }
+
+    private void registerUser () {
+        String email = txtEmail.getText().toString().trim();
+        String fullName = txtFullName.getText().toString().trim();
+        String age = txtAge.getText().toString().trim();
+        String password = txtPassword.getText().toString().trim();
+        String reenterPassword = txtReenterPassword.getText().toString().trim();
+
+        if (!validateData(email, fullName, age, password, reenterPassword)) {
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            User user = new User(fullName, age, email,
+                                    new ArrayList<Product>(),
+                                    new ArrayList<Recipe>(),
+                                    new ArrayList<Product>(),
+                                    new ArrayList<Product>()
+                            );
+                            FirebaseDatabase.getInstance().getReference("user")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(NewCustomerActivity.this, "You have been registered!",
+                                                Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+
+                                        //to complete
+                                    } else {
+                                        Toast.makeText(NewCustomerActivity.this, "Failed to register! Try again.",
+                                                Toast.LENGTH_SHORT).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+
+                        } else {
+                            Toast.makeText(NewCustomerActivity.this, "Failed to register! Try again.",
+                                    Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+    }
+
+    private boolean validateData(String email,
+                                 String fullName,
+                                 String age,
+                                 String password,
+                                 String reenterPassword) {
+        if (fullName.isEmpty()) {
+            txtFullName.setError("Full name is required.");
+            txtFullName.requestFocus();
+            return false;
+        }
+
+        if (age.isEmpty()) {
+            txtAge.setError("Age is required.");
+            txtAge.requestFocus();
+            return false;
+        }
+
+        if (email.isEmpty()) {
+            txtEmail.setError("Email is required.");
+            txtEmail.requestFocus();
+            return false;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            txtEmail.setError("Please provide a valid email address");
+            txtEmail.requestFocus();
+            return false;
+        }
+
+        if (password.isEmpty()) {
+            txtPassword.setError("Please provide a password.");
+            txtPassword.requestFocus();
+            return false;
+        }
+
+        if (password.length() < 6) {
+            txtPassword.setError("Password must be at least 6 characters.");
+            txtPassword.requestFocus();
+            return false;
+        }
+
+        if (!password.equals(reenterPassword)) {
+            txtReenterPassword.setError("Passwords must match.");
+            txtReenterPassword.requestFocus();
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/colockumhillsidefarmapp/user/User.java
+++ app/src/main/java/com/example/colockumhillsidefarmapp/user/User.java
@@ -1,26 +1,93 @@
 package com.example.colockumhillsidefarmapp.user;
 
 import com.example.colockumhillsidefarmapp.GlobalStorage;
+import com.example.colockumhillsidefarmapp.customer.recipes.Recipe;
+import com.example.colockumhillsidefarmapp.customer.store.Product;
+
+import java.util.ArrayList;
 
 public class User {
 
-    private TypeOfUser typeOfUser;
-    private static User instance;
-    private String user;
+    private String fullName, age, email;
+    private ArrayList<Product> wishlist;
+    private ArrayList<Recipe> favoriteRecipes;
+    private ArrayList<Product> favoriteProducts;
+    private ArrayList<Product> shoppingCart;
 
     public User() {
-        this.typeOfUser = null;
+
     }
 
-    public static User getInstance() {
-        if(instance == null){
-            instance = new User();
-        }
-        return instance;
+    public User(String fullName,
+                String age,
+                String email,
+                ArrayList<Product> wishlist,
+                ArrayList<Recipe> favoriteRecipes,
+                ArrayList<Product> favoriteProducts,
+                ArrayList<Product> shoppingCart
+    ) {
+        this.fullName = fullName;
+        this.age = age;
+        this.email = email;
+        this.wishlist = wishlist;
+        this.favoriteProducts = favoriteProducts;
+        this.favoriteRecipes = favoriteRecipes;
+        this.shoppingCart = shoppingCart;
     }
 
-    public void setCustomer(String user) {
-        this.user = user;
-        typeOfUser = TypeOfUser.CUSTOMER;
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Product> getWishlist() {
+        return wishlist;
+    }
+
+    public void setWishlist(ArrayList<Product> wishlist) {
+        this.wishlist = wishlist;
+    }
+
+    public ArrayList<Recipe> getFavoriteRecipes() {
+        return favoriteRecipes;
+    }
+
+    public void setFavoriteRecipes(ArrayList<Recipe> favoriteRecipes) {
+        this.favoriteRecipes = favoriteRecipes;
+    }
+
+    public ArrayList<Product> getFavoriteProducts() {
+        return favoriteProducts;
+    }
+
+    public void setFavoriteProducts(ArrayList<Product> favoriteProducts) {
+        this.favoriteProducts = favoriteProducts;
+    }
+
+    public ArrayList<Product> getShoppingCart() {
+        return shoppingCart;
+    }
+
+    public void setShoppingCart(ArrayList<Product> shoppingCart) {
+        this.shoppingCart = shoppingCart;
     }
 }
